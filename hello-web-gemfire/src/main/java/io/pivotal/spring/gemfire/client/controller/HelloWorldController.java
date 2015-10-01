d359977637ac22eede2e81d1d227600db05730fb
pulkitpivotal <pchandra@pivotal.io>
2015-10-01 14:18:19 -0400

    Fixed gradle build to include multiple repository which contains GemFire jars

--- hello-web-gemfire/src/main/java/io/pivotal/spring/gemfire/client/controller/HelloWorldController.java
+++ hello-web-gemfire/src/main/java/io/pivotal/spring/gemfire/client/controller/HelloWorldController.java
@@ -1,7 +1,21 @@
 package io.pivotal.spring.gemfire.client.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by pchandra on 10/1/15.
  */
+@Controller
 public class HelloWorldController {
+	
+	/**
+	 * Default page to return
+	 * 
+	 * @return name of the freemarker template
+	 */
+	@RequestMapping("/")
+	public String name() {
+		return "index";
+	}
 }
---
+++ hello-web-gemfire/src/main/resources/templates/index.ftl
@@ -0,0 +1,6 @@
+<html>
+<title>Welcome</title>
+<body>
+<h1>Hello !!</h1>
+</body>
+</html>
